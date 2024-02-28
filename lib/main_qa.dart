79d4f6f6b59f921dc04d47ad60b8cbb5808ecbe8
DANIS <danis.preldzic@sqa-consulting.com>
2024-02-28 14:08:10 +0100

    applied new gradle way kotlin version up gradle version up

--- lib/main_qa.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'app/MyApp.dart';
-import 'config/flavor_config.dart';
-
-void main() {
-  FlavorConfig(flavor: Flavor.QA, values: FlavorValues(appName: 'DjudjoInk QA'));
-  runApp(MyApp());
-}
